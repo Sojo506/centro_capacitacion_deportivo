6a344dab260ce5c1f53a1bbd991da895a1c4086e
Sojo506 <fsojodev@gmail.com>
2025-06-23 15:06:38 -0600

    create UserController to handle UserDAOImpl

---
+++ src/controller/UserController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import dao.user.UserDAO;
+import dao.user.UserDAOImpl;
+import model.User;
+import util.HashUtil;
+
+import java.util.List;
+
+public class UserController {
+
+    private UserDAO dao = new UserDAOImpl();
+
+    public void registerUser(String fullName, String nickname, String password) {
+        String hash = HashUtil.sha256(password);
+        User u = new User(fullName, nickname, hash, true);
+        dao.add(u);
+    }
+
+    public List<User> listUsers() {
+        return dao.getAll();
+    }
+
+    public boolean deactivateUser(String nickname) {
+        return dao.deactivate(nickname);
+    }
+
+    public User findUser(String nickname) {
+        return dao.findByNickname(nickname);
+    }
+}
