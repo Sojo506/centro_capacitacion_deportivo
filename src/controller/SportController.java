540705ecad69a43d644b6a9bcabbdd0cae894f33
Sojo506 <fsojodev@gmail.com>
2025-06-23 22:48:59 -0600

    create SportController with all its methods implemented

---
+++ src/controller/SportController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import dao.sport.SportDAO;
+import dao.sport.SportDAOImpl;
+import model.Sport;
+
+import java.util.List;
+
+public class SportController {
+
+    private SportDAO dao = new SportDAOImpl();
+
+    public boolean registerSport(Sport s) {
+        if (dao.findByName(s.getName()) != null) {
+            System.out.println("❌ Sport already exists.");
+            return false;
+        }
+        dao.add(s);
+        return true;
+    }
+
+    public void updateSport(Sport s) {
+        dao.update(s);
+    }
+
+    public boolean deactivateSport(int id) {
+        return dao.deactivate(id);
+    }
+
+    public Sport getSportById(int id) {
+        return dao.findById(id);
+    }
+
+    public List<Sport> listSports() {
+        return dao.getAll();
+    }
+}
