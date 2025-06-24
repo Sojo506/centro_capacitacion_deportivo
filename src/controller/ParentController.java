0058d0ac93bce58e59cd549a101de92703ee40cd
Sojo506 <fsojodev@gmail.com>
2025-06-23 22:37:10 -0600

    create ParentController to use ParentDAOImpl logic

---
+++ src/controller/ParentController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import dao.parent.ParentDAO;
+import dao.parent.ParentDAOImpl;
+import dao.athlete.AthleteDAO;
+import dao.athlete.AthleteDAOImpl;
+import model.Parent;
+import model.Athlete;
+
+import java.util.List;
+
+public class ParentController {
+
+    private ParentDAO parentDAO = new ParentDAOImpl();
+    private AthleteDAO athleteDAO = new AthleteDAOImpl();
+
+    public boolean registerParent(Parent parent) {
+        Athlete a = athleteDAO.findById(parent.getAthleteId());
+        if (a == null) {
+            System.out.println("Athlete does not exist.");
+            return false;
+        }
+        parentDAO.add(parent);
+        return true;
+    }
+
+    public void updateParent(Parent parent) {
+        parentDAO.update(parent);
+    }
+
+    public boolean deactivateParent(int id) {
+        return parentDAO.deactivate(id);
+    }
+
+    public Parent getParentById(int id) {
+        return parentDAO.findById(id);
+    }
+
+    public Parent getParentByEmail(String email) {
+        return parentDAO.findByEmail(email);
+    }
+
+    public List<Parent> listParents() {
+        return parentDAO.getAll();
+    }
+}
