874d68d4e3a238b5f0c3d35ff796090f6fd54911
Sojo506 <fsojodev@gmail.com>
2025-06-23 21:26:42 -0600

    create Athlete class with its constructors and attributes

---
+++ src/model/Athlete.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Athlete extends Person {
+
+    private int id;
+
+    public Athlete() {
+    }
+
+    public Athlete(int id, String name, String lastName, String city, String address,
+            String phone, String email, boolean active) {
+        super(name, lastName, city, address, phone, email, active);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
