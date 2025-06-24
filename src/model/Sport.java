e32ed865956482606dba8c20febe922df948bf96
Sojo506 <fsojodev@gmail.com>
2025-06-23 21:28:01 -0600

    create Sport class with its constructors and attributes

---
+++ src/model/Sport.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Sport {
+
+    private int id;
+    private String name;
+    private String characteristics;
+    private boolean active;
+
+    public Sport() {
+    }
+
+    public Sport(int id, String name, String characteristics, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.characteristics = characteristics;
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacteristics() {
+        return characteristics;
+    }
+
+    public void setCharacteristics(String characteristics) {
+        this.characteristics = characteristics;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+}
