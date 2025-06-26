4cde1edffc2a64c313fdc5a4f6f164b58d8bf340
Sojo506 <fsojodev@gmail.com>
2025-06-26 12:07:45 -0600

    create InvoiceController with its methods

---
+++ src/controller/InvoiceController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import dao.invoice.InvoiceDAO;
+import dao.invoice.InvoiceDAOImpl;
+import dao.athlete.AthleteDAO;
+import dao.athlete.AthleteDAOImpl;
+import dao.routine.RoutineDAO;
+import dao.routine.RoutineDAOImpl;
+
+import model.Invoice;
+import model.Athlete;
+import model.Routine;
+
+import java.util.List;
+
+public class InvoiceController {
+
+    private InvoiceDAO dao = new InvoiceDAOImpl();
+    private AthleteDAO athleteDAO = new AthleteDAOImpl();
+    private RoutineDAO routineDAO = new RoutineDAOImpl();
+
+    public boolean registerInvoice(Invoice invoice) {
+        // Validar atleta
+        Athlete a = athleteDAO.findById(invoice.getAthleteId());
+        if (a == null || !a.isActive()) {
+            System.out.println("Athlete not found or inactive.");
+            return false;
+        }
+
+        // Validar rutinas
+        for (Routine r : invoice.getRoutines()) {
+            Routine routineDB = routineDAO.findById(r.getId());
+            if (routineDB == null || !routineDB.isActive()) {
+                System.out.println("Routine ID " + r.getId() + " not found or inactive.");
+                return false;
+            }
+        }
+
+        dao.create(invoice);
+        return true;
+    }
+
+    public List<Invoice> listInvoices() {
+        return dao.getAll();
+    }
+
+    public Invoice getInvoiceById(int id) {
+        return dao.findById(id);
+    }
+
+    public boolean deactivateInvoice(int id) {
+        return dao.deactivate(id);
+    }
+}
