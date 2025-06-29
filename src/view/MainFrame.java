3ba056594109b0a0216239c52bfcd6bbb123c5f2
Sojo506 <fsojodev@gmail.com>
2025-06-28 20:55:13 -0600

    add User param to the constructor

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -2,15 +2,20 @@ package view;
 
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import model.User;
 
 public class MainFrame extends javax.swing.JFrame {
 
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(MainFrame.class.getName());
     private JPanel panelContent;
+    private User user;
 
-    public MainFrame() {
+    public MainFrame(User user) {
+        setUndecorated(true);
         initComponents();
         setLocationRelativeTo(null);
+        this.user = user;
+        System.out.println(user.getFullName());
     }
 
     private void showPanel(JPanel newPanel) {
@@ -25,32 +30,17 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        mainPanel1 = new view.panel.MainPanel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        mainPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        javax.swing.GroupLayout mainPanel1Layout = new javax.swing.GroupLayout(mainPanel1);
-        mainPanel1.setLayout(mainPanel1Layout);
-        mainPanel1Layout.setHorizontalGroup(
-            mainPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1200, Short.MAX_VALUE)
-        );
-        mainPanel1Layout.setVerticalGroup(
-            mainPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 700, Short.MAX_VALUE)
-        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(mainPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 1200, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(mainPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 700, Short.MAX_VALUE)
         );
 
         pack();
@@ -76,10 +66,10 @@ public class MainFrame extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new MainFrame().setVisible(true));
+        User user = new User();
+        java.awt.EventQueue.invokeLater(() -> new MainFrame(user).setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private view.panel.MainPanel mainPanel1;
     // End of variables declaration//GEN-END:variables
 }
