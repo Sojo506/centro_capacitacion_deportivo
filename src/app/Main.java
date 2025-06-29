0621f2726d4d77ba64ba9dc3be33941421c3d206
Sojo506 <fsojodev@gmail.com>
2025-06-29 12:06:00 -0600

    call LoginFrame

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,13 @@
 package app;
 
+import view.login.LoginFrame;
+
 public class Main {
 
     public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
     }
 
 }
