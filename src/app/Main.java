850f23224d1e89bfa4cdcc847943194babb98fb0
Sojo506 <fsojodev@gmail.com>
2025-06-30 18:30:02 -0600

    add nimbus as UIManager

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,26 @@
 package app;
 
 import view.login.LoginFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Mostrar ventana
         java.awt.EventQueue.invokeLater(() -> {
             new LoginFrame().setVisible(true);
         });
     }
-
 }
