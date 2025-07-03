6602cc7a438bba62fcd0970151c6b661799c5cca
Sojo506 <fsojodev@gmail.com>
2025-07-03 12:46:57 -0600

    create SportDialog

---
+++ src/view/sport/SportDialog.java
@@ -0,0 +1,35 @@
+package view.sport;
+
+import model.Sport;
+
+public class SportDialog extends javax.swing.JDialog {
+
+    public SportDialog(java.awt.Frame parent, SportPanel sportPanel, boolean modal, Sport sport) {
+        super(parent, modal);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
